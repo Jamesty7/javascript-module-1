68a24e9751b63b04c2b8a6d53bed3909a9a60593
Jamesty7 <jamestysoto@gmail.com>
2022-02-27 10:50:05 +0100

    Last part of inClass done

---
+++ week-1/InClass/exercise-I.js
@@ -0,0 +1,9 @@
+function whenIsYourBd(age) {
+    let currentYear = 2022
+    let yearOfBirth = currentYear - age
+    return yearOfBirth
+}
+
+console.log(`Juan: ${whenIsYourBd(28)}`)
+console.log(`Mirna: ${whenIsYourBd(26)}`)
+console.log(`Mari: ${whenIsYourBd(23)}`)
